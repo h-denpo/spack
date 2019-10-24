be32a920a35094b00d1ddfd2ac8656fd572f4eab
Satish Balay <balay@mcs.anl.gov>
2019-10-24 18:48:20 -0500

    enable mfem+sundials enable sundials+petsc+superlu_dist remove hypre~internal-superlu

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -37,14 +37,14 @@ class Xsdk(BundlePackage):
             description='Enable precice package build')
     variant('butterflypack', default=True, description='Enable butterflypack package build')
 
-    depends_on('hypre@develop~internal-superlu+superlu-dist+shared', when='@develop')
-    depends_on('hypre@2.18.2~internal-superlu+superlu-dist+shared', when='@0.5.0')
+    depends_on('hypre@develop+superlu-dist+shared', when='@develop')
+    depends_on('hypre@2.18.2+superlu-dist+shared', when='@0.5.0')
     depends_on('hypre@2.15.1~internal-superlu', when='@0.4.0')
     depends_on('hypre@2.12.1~internal-superlu', when='@0.3.0')
     depends_on('hypre@xsdk-0.2.0~internal-superlu', when='@xsdk-0.2.0')
 
     depends_on('mfem@develop+mpi+hypre+superlu-dist+petsc~sundials+examples+miniapps', when='@develop')
-    depends_on('mfem@4.0.1-xsdk+mpi+hypre+superlu-dist+petsc~sundials+examples+miniapps', when='@0.5.0')
+    depends_on('mfem@4.0.1-xsdk+mpi+hypre+superlu-dist+petsc+sundials+examples+miniapps', when='@0.5.0')
     depends_on('mfem@3.4.0+mpi+hypre+superlu-dist+petsc+sundials+examples+miniapps', when='@0.4.0')
     depends_on('mfem@3.3.2+mpi+hypre+superlu-dist+petsc+sundials+examples+miniapps', when='@0.3.0')
 
@@ -97,8 +97,8 @@ class Xsdk(BundlePackage):
     depends_on('alquimia@xsdk-0.2.0', when='@xsdk-0.2.0')
 
     depends_on('sundials+superlu-dist', when='@0.5.0: %gcc@6.1:')
-    depends_on('sundials@develop~int64+hypre', when='@develop')
-    depends_on('sundials@5.0.0~int64+hypre', when='@0.5.0')
+    depends_on('sundials@develop~int64+hypre+petsc', when='@develop')
+    depends_on('sundials@5.0.0~int64+hypre+petsc', when='@0.5.0')
     depends_on('sundials@3.2.1~int64+hypre', when='@0.4.0')
     depends_on('sundials@3.1.0~int64+hypre', when='@0.3.0')
 
@@ -111,10 +111,10 @@ class Xsdk(BundlePackage):
     depends_on('magma@2.4.0', when='@0.4.0 +cuda')
     depends_on('magma@2.2.0', when='@0.3.0 +cuda')
 
-    depends_on('amrex@develop+sundials', when='@develop %intel')
-    depends_on('amrex@develop+sundials', when='@develop %gcc')
-    depends_on('amrex@19.08+sundials', when='@0.5.0 %intel')
-    depends_on('amrex@19.08+sundials', when='@0.5.0 %gcc')
+    depends_on('amrex@develop', when='@develop %intel')
+    depends_on('amrex@develop', when='@develop %gcc')
+    depends_on('amrex@19.08', when='@0.5.0 %intel')
+    depends_on('amrex@19.08', when='@0.5.0 %gcc')
     depends_on('amrex@18.10.1', when='@0.4.0 %intel')
     depends_on('amrex@18.10.1', when='@0.4.0 %gcc')
 
